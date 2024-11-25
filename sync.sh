1a8d0e2e9a044d3384d895f7eb8108d49a6b91ed
psytrap <psytrap@github.com>
2024-11-25 17:47:21 +0100

    sync script + favicon

---
+++ sync.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mv -v ./src/every-smart.html ./docs/index.html
+mv -v ./src/every-smart.js ./docs/every-smart.js
+mv -v ./src/app.min.css ./docs/app.min.css
+mv -v ./src/dark.min.css ./docs/dark.min.css
+mv -v ./src/images/every-smart-16.png ./docs/images/every-smart-16.png
